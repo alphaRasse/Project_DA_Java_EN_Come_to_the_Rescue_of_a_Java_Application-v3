74e819634f9b49f297b2de9003a076b49b2b64f8
Yazid IGGUI <Vous@exemple.comDizayb1@hotmail.comDizayb1@hotmail.com>
2021-07-17 20:56:41 +0200

    code cleaning and correction of 'headache', 'rash', 'dialated pupils' values

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,65 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;	
+	private static int rashCount = 0;		
+	private static int pupilCount = 0;		
 
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
-
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+	public static void main(String args[])  {
+		try {
+			
+			
+		/**
+		 * method of reading the file "symptoms txt" 
+		 * and which has three examples of number of occurrences
+		 */
+		 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		 
+		String line = reader.readLine();	
+	
 		while (line != null) {
-			i++;	// increment i
+			
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+				headacheCount++;
+				System.out.println("number of headaches: " + headacheCount);
 			}
-			else if (line.equals("rush")) {
+			else if (line.equals("rash")) {
 				rashCount++;
 			}
 			else if (line.contains("pupils")) {
 				pupilCount++;
 			}
 
-			line = reader.readLine();	// get another symptom
+			line = reader.readLine();
+			
 		}
+		
+	
 
-		// next generate output
+		/**
+		 * method of creating the file "result.out" 
+		 * and writing number of occurrences of three symptoms 
+		 * "headache","rash","dialated pupils"
+		 */
+		
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
 		writer.close();
+		reader.close();
+	} catch (FileNotFoundException e) {
+		
+		System.err.printf("the %s file does not exist!","symptoms.txt");
+	} catch(IOException e) {
+		System.out.println("a problem occurred when the tomporary drive was closed");
 	}
+		}
 }
