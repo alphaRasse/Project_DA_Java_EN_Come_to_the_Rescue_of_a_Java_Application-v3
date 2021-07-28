f5e68a7bc9fff12ba10f52a1e0ec6a39673d6027
Yazid IGGUI <Vous@exemple.comDizayb1@hotmail.comDizayb1@hotmail.com>
2021-07-28 15:50:01 +0200

    new version on branch P003-v3 redefining as short as possible the valueOfSymptoms method

--- src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -12,6 +12,7 @@ package com.hemebiotech.analytics;
 	import java.io.IOException;
 	import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 	import java.util.TreeMap;
@@ -19,15 +20,9 @@ import java.util.Map;
 	public class ReadSymptomDataFromFile implements ISymptomReader {
 		
 		private String filepath;
-		List<Integer> occurrences = new ArrayList<Integer>();
-		List<String> symptoms = new ArrayList<String>();
-		Map<String,Integer> result1 = new TreeMap<String,Integer>();
-		
 		ArrayList<String> result = new ArrayList<String>();
-
-		
-		
-		
+		Map<String,Integer> result1 = new TreeMap<String,Integer>();
+				
 
 		public ReadSymptomDataFromFile(String filepath) {
 			
@@ -50,61 +45,47 @@ import java.util.Map;
 					}
 					reader.close();
 					
-					
 				} catch (FileNotFoundException e) {
-					
+				
 					System.err.printf("the %s file does not exist!",filepath);
 				} catch(IOException e) {
 					System.out.println("a problem occurred when the tomporary drive was closed");
-				}
-				 
-									}	
+			}
+				//Collections.sort(result);
+								}	
 			return result;
 		}
 		
-
+		
 		/**
-		 * valueOfsymptoms() allows the program to count the number of times n symptom is repeated
+		 * valueofsymptoms() allows the program to count the number of times n symptom is repeated
 		 */
 		@Override
 		public void valueOfSymptoms() {
-
-			for(String s : result) {
-				if (symptoms.contains(s))
-				{
-					int n = symptoms.indexOf(s);
-	                
-					occurrences.set(n, occurrences.get(n)+1);				
-				}
-				else
-				{
-					symptoms.add(s);
-					occurrences.add(1);
-				}
-			}
+			/**
+			 *  enregister les elements du tableau[] dans la treemap avec la methode frequency 
+			 */
 			
-			for(int i = 0; i < symptoms.size(); i++)
-			{
-	 
-			result1.put(symptoms.get(i) ,occurrences.get(i));
-			   
-		}
+			for(String s : result) {
+				result1.put(s, Collections.frequency(result, s));
+				
 			}
+		}
 		
-
+		
 		/**
 		 * The fileResultOut() method allows the program to create and write a file named result.out
 		 */
 		@Override
 		public void fileResultOut() {
-			
+			PrintWriter writer;
 			try {
-				PrintWriter writer = new PrintWriter(new FileWriter("result1.out"));
+				writer = new PrintWriter(new FileWriter("result2.out"));
 				
 				for(Map.Entry<String, Integer> m : result1.entrySet()) {
 					if(m.getKey() != null) {
 						String symptom = m.getKey();
-						int value = m.getValue();					
+						int value = m.getValue();
 							writer.println(symptom+" = "+value);
 					}
 				}
@@ -113,9 +94,10 @@ import java.util.Map;
 			
 				System.out.println("a problem occurred when closing PrintWriter");
 			}
-		
 			
 		}
+
+
 		
 		
 	}
