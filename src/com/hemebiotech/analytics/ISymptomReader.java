3e435c60bf92df16eb57b7f1d5d13220072639b5
Yazid IGGUI <Vous@exemple.comDizayb1@hotmail.comDizayb1@hotmail.com>
2021-08-07 17:32:33 +0200

    correction

--- src/com/hemebiotech/analytics/ISymptomReader.java
+++ src/com/hemebiotech/analytics/ISymptomReader.java
@@ -10,19 +10,11 @@ public interface ISymptomReader {
 	 *  @return
 	 * a raw listing of all Symptoms obtained from a data source, duplicates are possible/probable
 	 */
-	public List<String> GetSymptoms() ;
+	public List<String> getSymptoms() ;
 	
 	
-	/**
-	 * @return this method returns the list by number of occurrences per symptom
-	 */
-	public void valueOfSymptoms() ;
 	
 	
-	/**
-	 * this method allows us to create the result.out file 
-	 * and write the symptoms and their occurrences in alphabetical order.
-	 */
-	public void fileResultOut();
+
 
 }
